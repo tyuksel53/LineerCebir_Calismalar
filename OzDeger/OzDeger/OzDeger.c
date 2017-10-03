bc0e5d900e73e811709c43d2d3dc77e048e4ede9
Taha Yüksel <taha.yuksel53@hotmail.com>
2017-10-04 01:27:07 +0300

    random number generating function has added
    bakın aha yandı

--- OzDeger/OzDeger/OzDeger.c
+++ OzDeger/OzDeger/OzDeger.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 #define SIZE 10;
-void yaz(int boyut, int matrix[4][4])
+int *rastgeleSayi_Uret(int sayiMiktari);
+/*void yaz(int boyut, int matrix[4][4])
 {
 	for (int i = 0; i < boyut; i++)
 	{
@@ -12,20 +14,30 @@ void yaz(int boyut, int matrix[4][4])
 				printf("%d ", matrix[i][j]);
 
 		}
-		printf("\n");as
+		printf("\n");
 	}
 }
-void determinat_bul(int boyut, int matrix[4][4],int satir[],int sutun[],int division)
+void determinat_bul(int boyut, int matrix[4][4],int satir ,int sutun,int division)
 {
-	int i, j;
+	int i, j,k,t;
 	if (division==boyut)
 	{
 		for (i = 0; i < boyut; i++)
 		{
 			for (j = 0; j < boyut; j++)
 			{
-				satir[0] = i;
-				sutun[0] = j;
+				int *arr = (int *)malloc(boyut-1 * boyut-1 * sizeof(int));
+				for (k = 0; k < boyut; k++)
+				{
+					for ( t = 0; t < boyut; t++)
+					{
+						if (k != i && t != j)
+						{
+
+						}
+					}
+				}
+				for
 				determinat_bul(boyut, matrix, satir, sutun,division-1);
 			}
 			printf("\n");
@@ -39,7 +51,7 @@ void determinat_bul(int boyut, int matrix[4][4],int satir[],int sutun[],int divi
 			{ 
 				for (int k = 0; k < boyut - division; k++)
 				{
-					if (satir[] != i && sutun != j)
+					if (satir != i && sutun != j)
 					{
 						printf("%d ", matrix[i][j]);
 					}
@@ -55,12 +67,12 @@ void determinat_bul(int boyut, int matrix[4][4],int satir[],int sutun[],int divi
 	}
 	
 }
-
+*/
 int main()
 {
-	printf("Hello World\n");
+	///printf("Hello World\n");
 	//int x=5;
-	int i;
+	/*int i;
 	int tmp = -999;
 	int terim1 = -6, terim2 = -2;
 	printf("x^2 %dx %d\n", terim2 + terim1, terim1*terim2);
@@ -100,8 +112,39 @@ int main()
 	}
 	int dizi[2] = { 0,0 };
 	//yaz(4, matrix);
-	determinat_bul(4,matrix,0,0,4);
+	determinat_bul(4,matrix,0,0,4);*/
+
+	printf("Kac tane sayi uretmek istiyorsunuz ?\n");
+	
+	int rastgeleMiktar = 0;
+	printf("---> ");
+	scanf_s("%d", &rastgeleMiktar);
+	
+	printf("\n");
+	
+	int *rastgele_sayilar;
+	
+	rastgele_sayilar = rastgeleSayi_Uret(rastgeleMiktar);
+
+	printf("\nRastgele sayilarin ilk terimi: %d\n", *(rastgele_sayilar + 0));
+
+	printf("\nRastgele sayilarin son terimi: %d\n\n", *(rastgele_sayilar + rastgeleMiktar -1) );
+
 	system("PAUSE");
 	return 0;
 }
+int *rastgeleSayi_Uret(int sayiMiktari)
+{
+	int i = 0;
+	srand(time(NULL));
+	int *rastgeleSayilar = malloc(sizeof(int) * sayiMiktari);
+	for (i = 0; i<sayiMiktari; i++)
+	{
+		rastgeleSayilar[i] = rand() % 241;
+		printf("%d ", rastgeleSayilar[i] ) ;
+	}
+	printf("\n");
+	return rastgeleSayilar;
+}
+
 
