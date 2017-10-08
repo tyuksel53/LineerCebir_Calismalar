1b940f05754e1398167f6ba3694017a2adec2da4
Taha Yüksel <taha.yuksel53@hotmail.com>
2017-10-08 16:47:07 +0300

    SubMatrix added another .c file
    deleted unnecessary codes.commend lines added.Sub matrix find takes time so be patient and wait.And don't forget the change file path.

---
+++ OzDeger/OzDeger/AltMatris_Source.c
@@ -0,0 +1,84 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/*
+	author: tyuksel
+	date:	8.10.2017
+*/
+void file_write(int boyut, int **matris)
+{
+	char str[200] = "C:\\users\\taha\\desktop\\"; // dosya yolu
+	char snum[12];
+	sprintf(snum, "%d", boyut); // boyuta göre ayrý dosyaya yaz
+	strcat(str, snum);
+	strcat(str, "submatrix.txt");
+
+	FILE *fp; // dosya aç
+	fp = fopen(str, "a");
+
+	int i, j;
+	for (i = 0; i < boyut; i++)
+	{
+		for (j = 0; j < boyut; j++)
+		{
+			fprintf(fp, "%d ", matris[i][j]);
+
+		}
+		fprintf(fp, "\n");
+	}
+	fprintf(fp, "\n");
+	fclose(fp); // dosya kapa
+}
+void alt_matris(int MatrisBoyut, int subBoyut, int **matris, int SiLsatir, int SiLsutun) // alt matris bulan recusive fonksiyon
+{
+	int i, j;
+	int **suBmatris = (int **)malloc(sizeof(int) * subBoyut - 1); // alt matrisi oluþtur
+	int satir = 0;
+	int sutun = 0;
+
+	for (i = 0; i < subBoyut; i++) // alt matrisi tanýmla
+	{
+		suBmatris[i] = (int *)malloc(sizeof(int)* subBoyut - 1);
+	}
+
+	for (i = 0; i < MatrisBoyut; i++)
+	{
+		for (j = 0; j < MatrisBoyut; j++)
+		{
+			if (i != SiLsatir && j != SiLsutun) // satýr ve sutunu sil
+			{
+				suBmatris[satir][sutun] = matris[i][j];
+				satir++;
+			}
+		}
+		if (satir == subBoyut)
+		{
+			satir = 0;
+			sutun++;
+		}
+	}
+
+	file_write(subBoyut, suBmatris); // dosyaya yaz
+
+	if (subBoyut != 2) // 2x2 matris oluncaya kadar ilerle
+	{
+		for (i = 0; i < subBoyut; i++)
+		{
+			for (j = 0; j < subBoyut; j++)
+			{
+				alt_matris(subBoyut, subBoyut - 1, suBmatris, i, j); // yukarýda oluþturulan alt matrisi tekrar çaðýr
+			}
+		}
+	}
+}
+void alt_matrisBul(int boyut, int **matris)
+{
+	int i, j;
+	int satir, sutun;
+	for (i = 0; i < boyut; i++)
+	{
+		for (j = 0; j < boyut; j++)
+		{
+			alt_matris(boyut, boyut - 1, matris, i, j);
+		}
+	}
+}
--- OzDeger/OzDeger/OzDeger.c
+++ OzDeger/OzDeger/OzDeger.c
@@ -3,102 +3,10 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-#define DOSYA_YOLU = "C:\\users\\taha\\desktop\\"
 
 int *rastgeleSayi_Uret(int sayiMiktari);
 
-void ekrana_yaz(int boyut, int **matris)
-{
-	char str[200] = "C:\\users\\taha\\desktop\\";
-	char snum[12];
-	sprintf(snum, "%d", boyut);
-	strcat(str, snum);
-	strcat(str, "submatrix.txt");
-	FILE *fp;
-	fp = fopen(str, "a");
-	int i, j;
-	for (i = 0; i < boyut; i++)
-	{
-		for (j = 0; j < boyut; j++)
-		{
-			//printf("%d ", matris[i][j]);
-			fprintf(fp, "%d ",matris[i][j]);
 
-		}
-		//printf("\n");
-		fprintf(fp, "\n");
-	}
-	fprintf(fp, "\n");
-	fclose(fp);
-	//printf("\n\n------>Ekrana Yazildi\n\n");
-	
-}
-void **alt_matris(int MatrisBoyut,int subBoyut, int **matris,int SiLsatir,int SiLsutun)
-{
-	int i, j;
-	int **suBmatris = (int **)malloc(sizeof(int) * subBoyut - 1);
-	int satir = 0;
-	int sutun = 0;
-	for (i = 0; i < subBoyut; i++)
-	{
-		suBmatris[i] = (int *)malloc(sizeof(int)* subBoyut - 1);
-	}
-	for (i = 0; i < MatrisBoyut; i++)
-	{
-		for (j = 0; j < MatrisBoyut; j++)
-		{
-			if (i != SiLsatir && j != SiLsutun)
-			{
-				suBmatris[satir][sutun] = matris[i][j];
-				satir++;
-			}	
-		}
-		if (satir == subBoyut)
-		{
-			satir = 0;
-			sutun++;
-		}
-	}
-	ekrana_yaz(subBoyut, suBmatris);
-	if (subBoyut != 2)
-	{
-		for (i = 0; i < subBoyut; i++)
-		{
-			for (j = 0; j < subBoyut; j++)
-			{
-				alt_matris(subBoyut, subBoyut - 1, suBmatris, i, j);
-			}
-		}
-	}
-
-	//return suBmatris;
-}
-void alt_matrisBul(int boyut, int **matris)
-{
-	int i, j;
-	int satir, sutun;
-	for (i = 0; i < boyut; i++)
-	{
-		for (j = 0; j < boyut; j++)
-		{
-			////int **suBmatris;
-			///suBmatris =
-			alt_matris(boyut, boyut-1, matris, i, j);
-			/////////ekrana_yaz(boyut-1, suBmatris);
-		}
-		//printf("\n");
-	}
-
-	for (i = 0; i < boyut; i++)
-	{
-		for (j = 0; j < boyut; j++)
-		{
-			printf("%d ", matris[i][j]);
-		}
-		printf("\n");
-	}
-	
-}
 int main()
 {
 	int matris_boyut = 5;
